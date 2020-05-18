cff78a8cf966410faeb3dfc3cce4579d90567173
David Vollendroff <civilianemail@gmail.com>
2020-05-18 11:35:08 -0700

    first commit

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,23 +1,36 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
-
+        swap_holder = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = swap_holder
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    swaps = 1
+    while swaps > 0:
+        swaps = 0
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                swap_holder = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = swap_holder
+                swaps += 1
+        
 
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,10 @@
 def linear_search(arr, target):
-    # Your code here
+    if len(arr) == 0:
+        return -1
+
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +12,22 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    if len(arr) == 0:
+        return -1
 
-    # Your code here
-
+    indices = {'left': 0, 'right': len(arr) - 1, 'mid': (len(arr)-1)//2}
+    if indices['mid'] == target:
+        return indices['mid']
+    
+    while (indices['mid'] != target) and (indices['left'] != indices['right']):
+        print(f"left:{indices['left']} right: {indices['right']} mid: {indices['mid']} target: {target}")
+        if target == arr[indices['mid']]:
+            return indices['mid']
+        elif target < arr[indices['mid']]:
+            indices['right'] = indices['mid']
+            indices['mid'] = (indices['left'] + indices['right']) // 2
+        elif target > arr[indices['mid']]:
+            indices['left'] = indices['mid']
+            indices['mid'] = (indices['left'] + indices['right']) // 2 
 
     return -1  # not found
