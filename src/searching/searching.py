2812420e1bc03d7b01fc45f093f5d18b4d5cb27b
Elisabeth <elisabeth@github.com>
2020-06-03 16:01:32 -1000

    Iterative search.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,40 @@
-def linear_search(arr, target):
-    # Your code here
+"""
+Iterative search assignment.
+"""
 
+def linear_search(arr, target):
+    """
+    A straightforward linear search, left-to-right.
+    """
+    for index, item in enumerate(arr):
+        if item == target:
+            return index
 
     return -1   # not found
 
 
-# Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    """
+    An iterative implementation of binary search.
+    """
+    if len(arr) == 0:
+        return -1 # array empty
 
-    # Your code here
+    low = 0
+    high = len(arr)
 
+    while high > low:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            # Found.
+            return mid
+
+        if arr[mid] > target:
+            # Too high, search lower half.
+            high = mid
+
+        elif arr[mid] < target:
+            # Too low, search upper half.
+            low = mid + 1
 
     return -1  # not found
