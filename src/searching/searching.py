ede9b61d1d6448388118361a2dd3300237f40aa5
ash12hub <ash12hub@users.noreply.github.com>
2020-06-06 09:54:46 +0530

    Searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,34 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    current_array = arr
+    add_midpoint = 0
+    while len(current_array) > 1:
+        midpoint = 0
+        if (len(current_array) % 2) == 0:
+            midpoint = int(len(current_array) / 2)
+        else:
+            midpoint = int(len(current_array) / 2 + 1)
 
+        if current_array[midpoint] == target:
+            return add_midpoint + midpoint
+        elif target < current_array[midpoint]:
+            current_array = current_array[:midpoint]
+        else:
+            add_midpoint = midpoint + add_midpoint
+            current_array = current_array[midpoint:]
+
+    if len(current_array) == 1:
+        if current_array[0] == target:
+            return 0
 
     return -1  # not found
