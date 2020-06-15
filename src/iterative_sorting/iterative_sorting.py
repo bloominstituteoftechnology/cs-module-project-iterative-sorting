7e8e857c7453b53583006a447b89cf984fbcf3fd
tmbern <tmbern42@gmail.com>
2020-06-15 16:04:38 -0600

    day one sorting functions

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,11 +6,15 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
+        # if arr[i] > arr[i + 1]:
+        #     smallest_index = i + 1
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
 
         # TO-DO: swap
-        # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
