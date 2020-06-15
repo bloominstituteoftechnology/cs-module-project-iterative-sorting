1cccb0bdaba09fa06c130b7dc3079240922ad059
ph5500 <philfives@gmail.com>
2020-06-15 17:05:05 -0600

    tests passing for searching.py answered part of readme questions

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
-    # Your code here
+    for index, value in enumerate(arr):
+        print(index, value)
+        if value == target:
+            return index
 
 
     return -1   # not found
@@ -7,8 +10,22 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        
+        if arr[mid] == target:
+            return mid
+        
+        if arr[mid] < target:
+            left = mid + 1
+            
+        else: 
+            right = mid - 1
+      
 
 
     return -1  # not found
