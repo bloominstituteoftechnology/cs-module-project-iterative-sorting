edb59ac19be96247496cad3846a5fe70f1ac769a
Brianna Keune <briannakeune@gmail.com>
2020-06-16 10:07:48 -0500

    searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,25 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
 
     # Your code here
+    while left <= right:
+        mid = (left + right) // 2
 
+        if arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
     return -1  # not found
