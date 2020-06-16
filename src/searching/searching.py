9c57092e88512efc3e692f5e890a074ad7a76e8c
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-15 20:50:29 -0700

    finished searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,9 @@
 def linear_search(arr, target):
     # Your code here
     for i in range(len(arr)):
-        print(arr[i])
+        # print(arr[i])
         if arr[i] == target:
-            return arr[i]
+            return i
     return -1   # not found
 
 
@@ -11,6 +11,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr) - 1
+    #find mid
+    while left <= right:
+        mid = (left + right)//2
+        #if mid is equal to target
+        if arr[mid] == target:
+            return mid
 
-
+        # Check if we go left or right
+        if arr[mid] < target:
+            # if target is greater toss out right
+            left = mid + 1
+        else:
+            # if target is greater toss out right
+            right = mid - 1
     return -1  # not found
