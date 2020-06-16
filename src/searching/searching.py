7c8d2057291b2435978df714f446ac5066d455ac
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-16 12:45:39 +0100

    Binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,6 @@
 def linear_search(arr, target):
     # Your code here
 
-
     return -1   # not found
 
 
@@ -9,6 +8,19 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+        midpoint = start + (end - start) // 2
+        mid_item = arr[midpoint]
+
+        if target == mid_item:
+            return midpoint
+        elif target < mid_item:
+            end = midpoint - 1
+        else:
+            start = midpoint + 1
 
 
     return -1  # not found
