c54c21782167e9c31773f0b78ce27448e4391b6f
Hector <hectoremil21@hotmail.com>
2020-06-17 18:28:37 -0400

    Implement bubble sort

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,8 +8,19 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    min = 0
+    max = len(arr) - 1
+    # [0, 1, 2, 3, 4]
+
+    while min <= max:
+        middle = min + max
+        if arr[middle] == target:
+            return middle
+        if arr[middle] > target:
+            max = middle - 1
+        else:
+            min = middle + 1
 
 
     return -1  # not found
