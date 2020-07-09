84160840ca884e25aec0bece24fa7b140460710b
Michael Lowe <mrlowememphis@yahoo.com>
2020-07-08 20:55:37 -0500

    iterative and searching sorts completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,38 +1,31 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    # for i in range(0, len(arr) - 1):
-    
-    for i in range(1, len(arr))
+    for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-        
-          for j in range(i + 1, len(arr)):
+
+        for j in range(cur_index+1, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
-        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
         # TO-DO: swap
         # Your code here
 
-        arr[i], arr[smallest_index] = arr[smallest_index], arr[i] # 1, 0 = 0, 1
-        
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-# def bubble_sort(arr):
-#     # Your code here
+def bubble_sort(arr):
+    # Your code here
 
-      for i in range(len(arr)):
-        for j in range(len(arr) - i -1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-      return arr
+
+    return arr
+
 
 
 '''
