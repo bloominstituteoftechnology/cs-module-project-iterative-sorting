fe4bcf766dc879307cb1c76e29f8684677d50877
Surfsol <surfsol@protonamil.com>
2020-07-17 11:17:42 -0300

    algos in js

--- Codility/Arrays/rotateArray.py
+++ Codility/Arrays/rotateArray.py
@@ -31,9 +31,40 @@ def solution(A, K):
     return A
 
 
-print(solution([9,3,4,2,4, 90, 89], 5))
+#print(solution([9,3,4,2,4, 90, 89], 5))
 
 i = 4
 length = 5
 K = 23
-        
+
+
+
+def solution1(A, N):
+    #number of times to rotate
+    #for each time, rotate once, until required times
+    # if not the last number, rotate 1 position
+    # put last number to 0 position
+    #while N is > 0
+    
+    
+    while N > 0:
+        rotator = [0 for _ in range(len(A))]
+        for i in range(len(A)):
+            if i < len(A) - 1:
+                print('>', i)
+                rotator[i+1] = A[i] 
+                #A[i+1] = A[i]
+            elif i == len(A)-1:
+                print('==', i)
+                rotator[0] = A[i]
+                #A[0] = A[i]
+            print(rotator)
+            print('A',A)
+        N -= 1
+        A = rotator
+        print(rotator)
+        print('A',A)
+    return rotator
+
+
+print(solution1([9,3,4,2,4, 90, 89], 2))
