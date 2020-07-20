d7e0f54a8108cf18025d56d2c0dedf5ed4ebe623
ksemenza <ksemenza13@gmail.com>
2020-07-20 18:00:54 -0400

    completed MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,40 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        # for each element in current index+1 to length of array
+        for j in range(cur_index, len(arr)):
+            # if item is < smallest index, capture smallest index
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
         # Your code here
-
+        # update smallest and current
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
+
+
 def bubble_sort(arr):
+
     # Your code here
 
-
+    # Run through array
+    for i in range(len(arr)):
+        # go through items
+        for j in range(len(arr)-1-i):
+            # compare if greater
+            if arr[j] > arr[j+1]:
+                # swap
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +53,24 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
+
+
+def counting_sort(arr, maximum=-1):
     # Your code here
-
-
-    return arr
+    counter = []
+    for i in range(len(arr)):
+        if arr[i] < 0:
+            return 'Error, negative numbers not allowed in Count Sort'
+        if arr[i] > maximum:
+            maximum = arr[i]
+            counter += [0] * ((maximum + 1) - len(counter))
+        counter[arr[i]] += 1
+    for i in range(1, len(counter)):
+        counter[i] += counter[i - 1]
+    counter.insert(0, 0)
+    counter.pop()
+    new = [0] * len(arr)
+    for x in arr:
+        new[counter[x]] = x
+        counter[x] -= 1
+    return new
