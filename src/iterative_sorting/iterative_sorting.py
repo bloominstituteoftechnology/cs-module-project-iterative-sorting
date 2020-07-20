3764e8ff272ea948cc8360af1420ff7d0f1f5449
Christine Fletcher <chrismis79@gmail.com>
2020-07-20 11:34:50 -0500

    iterative search tests passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,31 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)-1):
         cur_index = i
-        smallest_index = cur_index
+        #smallest_index = cur_index
+            
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(i + 1, len(arr)):
+            if arr[cur_index] > arr[j]:
+                cur_index = j
 
         # TO-DO: swap
         # Your code here
-
+        arr[i], arr[cur_index] = arr[cur_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j +1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 '''
