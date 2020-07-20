55e5b2b31d616c44e183be357e6b64e84e0281af
Michael Famurewa <myfamurewa@gmail.com>
2020-07-20 15:52:53 -0400

    MVP completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,49 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)- 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        j = i
+        lowest = 1000000
+        lowest_index = j
+        while j <= len(arr) - 1:
+            if arr[j] < lowest:
+                lowest = arr[j]
+                lowest_index = j
+            j += 1
+
 
 
         # TO-DO: swap
+        arr[lowest_index], arr[cur_index] = arr[cur_index], lowest
         # Your code here
 
     return arr
 
-
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    moved = True
+    while moved:
+        moved = False
+        for i in range(0, len(arr) - 1):
+            right = i + 1
+            left = i
+            if arr[left] > arr[right]:
+                arr[right], arr[left] = arr[left], arr[right]
+                moved = True
+            
+    
+
 
 
     return arr
+
+print(bubble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]))
 
 '''
 STRETCH: implement the Counting Sort function below
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,22 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
-
+    for element in range (0, len(arr)):
+        if arr[element] == target:
+            return element
+    
+    return -1
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        middle = (low+high)//2
+        if target < arr[middle]:
+            high = middle - 1 # eliminate RHS
+        elif target > arr[middle]:
+            low = middle + 1 #eliminate LHS
+        else:
+            return middle
+    
+    return -1
 
-    # Your code here
-
-
-    return -1  # not found
