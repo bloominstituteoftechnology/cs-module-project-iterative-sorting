2bcc12390ce7955d41f4a364d07a30db79897664
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-20 18:59:14 -0400

    Completes searching.py, tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,13 @@
 def linear_search(arr, target):
+    """
+    Traverse the dataset one item at a time until either you find the item you are
+    looking for OR check every item in the set and verify the item you are
+    looking for is not there.
+    """
     # Your code here
-
+    for x in range(len(arr)):
+        if arr[x] == target:
+            return x
 
     return -1   # not found
 
@@ -13,14 +20,14 @@ def binary_search(arr, target):
     """
     # Your code here
     low = 0
-    high = len(arr) -1
+    high = len(arr) - 1
 
     while low <= high:
 
         # 1. Compare the target element to the midpoint
         mid = (high + low) // 2
 
-        # 2. If the midpoint element matches our target, then we've 
+        # 2. If the midpoint element matches our target, then we've
         #    we've found what we're looking for, return the midpoint
         #    index
         if target == arr[mid]:
@@ -31,10 +38,20 @@ def binary_search(arr, target):
         if target < arr[mid]:
             # cut out the right half
             # reassign high to mid -1
-            high = mid -1
+            high = mid - 1
         if target > arr[mid]:
             # cut out the left half
             # reassign low to mid + 1
-            low = mid +1
-        
+            low = mid + 1
+
     return -1  # not found
+
+
+if __name__ == "__main__":
+    my_list = [23, 34, 55, 67]
+
+    # print(linear_search(my_list, 2))
+
+    arr1 = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
+    print(linear_search(arr1, 7))
+    # breakpoint()
