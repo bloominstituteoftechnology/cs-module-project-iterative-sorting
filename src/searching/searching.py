8d9cb814b8edaf5a005bd489d4e7ee2785b350b6
Kelli Scheuble <github@scheuble.dev>
2020-07-20 16:15:24 -0700

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,32 @@
 def linear_search(arr, target):
-    # Your code here
+
+    for i, value in enumerate(arr):
+        if value == target:
+            return i
+
+    return -1  
 
 
-    return -1   # not found
+def binary_search(arr, value):
+    first = 0
+    last = (len(arr) - 1)
+​
+    found = -1
+​
+    while first <= last and found = -1:
+        # find middle using integer divsion
+        middle = (first + last) // 2
+​
+        if arr[middle] == value:
+            found = middle
+​
+        else:
+            if value < arr[middle]:
+                # search lower half of remainder
+                last = middle - 1
+            else:
+                # search upper half of remainder
+                first = middle + 1
+​
+    return found
 
-
-# Write an iterative implementation of Binary Search
-def binary_search(arr, target):
-
-    # Your code here
-
-
-    return -1  # not found
