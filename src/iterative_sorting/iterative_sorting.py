e3ca716dd069aa2733884945ac53d893c8d918fe
Wesley Mountford <goe_horus@hotmail.com>
2020-07-21 12:13:20 -0500

    Did monday's assignment.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,7 +6,7 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        for j in range(smallest_index + 1, len(arr)):
+        for j in range(cur_index + 1, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for index, i in enumerate(arr):
+        if target == i:
+            return index
 
 
     return -1   # not found
@@ -8,7 +10,16 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        mid = (high + low) // 2
 
+        if arr[mid] < target: #Target is higher than our checked value, set new low value.
+            low = mid + 1
+        elif arr[mid] > target:#Target is lower than our checked value, set new high value.
+            high = mid - 1
+        else: #Target is equal to arr[mid], return mid.
+            return mid
     return -1  # not found
