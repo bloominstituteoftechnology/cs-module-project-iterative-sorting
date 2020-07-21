7d883526cd98b708e4e82c44637846395873343d
Your name <a.m.verdine@gmail.com>
2020-07-21 19:50:36 -0400

    Completed binary search algorithm

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,20 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        middle = (low + high) // 2
+        guess = arr[middle]
+
+        if guess == target:
+            return middle
+        if guess > target:
+            high = middle - 1
+        else:
+            low =  middle + 1
 
 
     return -1  # not found
