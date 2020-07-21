333ba16382b127fe468cf1a835a912a2ae7a2c8f
Jllin50 <jack.lindberg.us@gmail.com>
2020-07-20 23:03:48 -0700

    basic iterative search algorithms found online

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,27 @@
 def linear_search(arr, target):
-    # Your code here
+    for x in range(0, len(arr)):
+        if arr[x] == target:
+            return x
+    return -1
 
 
-    return -1   # not found
 
-
-# Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    start = 0
+    end = len(arr)
 
-    # Your code here
+    if end == 0:
+        return -1
 
+    while start <= end:
+        mid = (start + end) // 2
 
-    return -1  # not found
+        if arr[mid] == target:
+            return mid
+
+        if target < arr[middle]:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+    return -1
