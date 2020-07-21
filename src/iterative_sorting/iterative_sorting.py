96b83ffe17dd61fe0d8a794812122e09ee6ce76f
Your name <a.m.verdine@gmail.com>
2020-07-21 19:33:35 -0400

    Completed bubble sort algorithm/Update README

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,7 +20,20 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
 
+    n = len(arr)
+    swapped = True
+
+    x= -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True    
 
     return arr
 
