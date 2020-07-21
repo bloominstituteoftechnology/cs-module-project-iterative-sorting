06620b13ffee4d81f966077be6f0e8ccb8dc1101
chrishuskey <chrishuskey@users.noreply.github.com>
2020-07-21 12:11:53 -0400

    Add completed insertion sort and selection sort functions.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,37 @@
-# TO-DO: Complete the selection_sort() function below
-def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+def insertion_sort(array):
+    """
+    Sort the input array using the insertion sort algorithm.
+    """
+    # For every unsorted item:
+    for i in range(1, len(array)):
+        value = array[i]
+        # Look "left" through sorted values until we find the right place to insert value:
+        j = i
+        while j > 0 and value < array[j-1]:
+            array[j] = array[j-1]
+            j -= 1
+        
+        # Insert the value at that place:
+        array[j] = value
+
+    return array
 
 
-        # TO-DO: swap
-        # Your code here
+def selection_sort(array):
+    # Search all unsorted elements and find the lowest value:
+    for i in range(0, len(array) - 1):
+        current_index = i
 
-    return arr
+        # Find next smallest element:
+        smallest_index = current_index
+        for j in range(current_index + 1, len(array)):
+            if array[j] < array[smallest_index]:
+                smallest_index = j
+        
+        # Swap the current value with the lowest value:
+        array[current_index], array[smallest_index] = array[smallest_index], array[current_index]
+    # Return sorted array:
+    return array
 
 
 # TO-DO:  implement the Bubble Sort function below
