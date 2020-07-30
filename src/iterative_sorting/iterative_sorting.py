5b843d9f212d072b2938ca433bcba257818bc87a
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-07-30 15:22:13 -0600

    iterative sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,9 +4,20 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        
         # TO-DO: find next smallest element
+        # will loop through the rest of the array and if I 
+        # find one smaller I will make the smallest index point at 
+        # the index that is the smallest
         # (hint, can do in 3 loc)
-        # Your code here
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                # reassinging the smallest index
+                smallest_index = j
+        # doing the swap
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
 
         # TO-DO: swap
@@ -17,7 +28,18 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    hasSwaped = True
+    while hasSwaped:
+        # setting hasSwaped to false if there was no swap
+        hasSwaped = False
+        # looping through the array
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                # putting that there was a swap
+                hasSwaped = True
 
 
     return arr
@@ -40,7 +62,38 @@ buckets.
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
-    # Your code here
-
+    # checking to see if there is a empty list
+    if not arr:
+        return arr
+    # checking to see if the maximum = none
+    if not maximum:
+        maximum  = arr[0]
+        for i in range(len(arr)):
+            if arr[i] > maximum:
+                maximum = arr[i]
+    # creating the array of the buckets
+    bucket_arry = [0] * (maximum + 1)
+   
+    # Looping through the array an counting the number of times
+    # that the i is seen in the arr
+    for i in range(len(arr)):
+        # adding up the occurences of the elements
+        if arr[i] >= 0: # making sure that it is not negative numbers
+            bucket_arry[arr[i]] += 1
+        else:
+            # going to stop and return a string that 
+            # says that can't have negative numbers
+            return "Error, negative numbers not allowed in Count Sort"
+    # now will be looping through and then adding
+    # up to find the index where the items starts
+    
+    for i in range(len(bucket_arry)-1):
+        
+        bucket_arry[i + 1] = bucket_arry[i] + bucket_arry[i+1]
+    # now filling the arr with the values
+    # doing the first line before doing a loop
+    arr[:bucket_arry[1]] = [0] * bucket_arry[1]
+    for i in range(len(bucket_arry)-1):
+        arr[bucket_arry[i]:bucket_arry[i+1]] = [i+1] * abs(bucket_arry[i] - bucket_arry[i+1])
 
     return arr
