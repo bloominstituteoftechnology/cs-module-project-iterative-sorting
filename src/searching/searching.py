648146b66adc130f1f6e46c3953b4deee021b91f
David Wright <davidtwright89@gmail.com>
2020-08-05 00:45:02 -0400

    Implemented binary_search()

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,8 +8,18 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    first = 0
+    last = len(arr) - 1
 
-    # Your code here
-
+    while first <= last:
+        middle = (first + last) // 2
+    
+        if arr[middle] == target:
+            return middle
+        else:
+            if arr[middle] > target:
+                last = middle - 1
+            else:
+                first = middle + 1
 
     return -1  # not found
