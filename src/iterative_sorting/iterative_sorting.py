b80dcde29bed21734f634c27d6c756b3811c8407
connor sanderford <connorsanderford@gmail.com>
2020-08-05 20:32:06 -0700

    tests pass

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,17 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
+        for j in range(i,len(arr)):
+            smallest_index = j if (arr[j] < arr[smallest_index]) else smallest_index
 
         # TO-DO: swap
-        # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+
 
     return arr
 
@@ -18,6 +19,13 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    swapped = True
+    while swapped == True:
+        swapped = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i+1], arr[i] = arr[i], arr[i+1]
+                swapped = True
 
 
     return arr
@@ -41,6 +49,36 @@ What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
     # Your code here
+    
+    if len(arr) == 0:
+        return []
+
+    if maximum is None:
+        maximum = max(arr)
+
+    if len([x for x in arr if x < 0]) > 0:
+        return "Error, negative numbers not allowed in Count Sort"
+
+
+    counts = [0] * (maximum + 1)
+    for i in range(len(counts)):
+        for j in range(len(arr)):
+            if i == arr[j]:
+                counts[i] += 1
+    
+
+    arr = []
+    for k in range(len(counts)):
+        if counts[k] > 0:
+            arr.extend([k] * counts[k])
 
 
     return arr
+
+if __name__ == "__main__":
+    import random
+
+    arr4 = random.sample(range(200), 50)
+    print(arr4)
+    print(" ")
+    print(counting_sort(arr4))
