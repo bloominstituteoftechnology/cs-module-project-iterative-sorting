2d453a9fa1bbf240416bc157bf6ad9a1f5a7cc1f
Jessie <jessie.griffin@icloud.com>
2020-08-08 14:10:55 -0400

    Binary passes

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,24 +1,40 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            # found
+            return target
+        # not found
+        return -1
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    first = 0
+    last = len(arr) - 1
 
+    while first <= last:
 
-    return -1  # not found
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            # return index of target not the value
+            return middle
+        else:
+            if target < arr[middle]:
+                # if target is less than the value of the mid element, make the element before the middle the last element to search through
+                last = middle - 1
+            else:
+                # if the target is greater than the middle element, make one after the middle element the starting point for the next search
+                first = middle + 1
+    # Same as returning false
+    return -1
 
 import random
 import time
 # 100,000 --> 50,000 Log(100,000)
 # linear search
 # keywords: unsorted and random
-
+"""
 my_range = 100
 my_size = 10
 
@@ -34,10 +50,11 @@ def linear_search(arr, target):
         return False
 
 print(linear_search(my_random, searching_for))
+"""
 
 # binary search
 # keywords: sorted, ordered
-
+"""
 def find_value_binary(arr, value):
     first = 0
     last = len(arr)-1
@@ -56,4 +73,4 @@ def find_value_binary(arr, value):
                 first = middle + 1
     return found
 print(find_value_binary(my_random, searching_for))
-
+"""
