d25231c4c69145cf21ec16850b4f40bbe632c01e
Denis Cedeno <denis.cedeno@gmail.com>
2020-08-07 20:50:22 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,9 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)): 
+  
+        if arr[i] == target: 
+            return i 
 
     return -1   # not found
 
@@ -9,6 +12,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
+
+        if target == arr[mid]:
+            return mid
+
+        if target < arr[mid]:
+            high = mid - 1
+        elif target > arr[mid]:
+            low = mid + 1
 
     return -1  # not found
