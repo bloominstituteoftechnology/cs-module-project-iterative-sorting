d82a5e9a5a3ae75f479cf4bcfc6062762e3d61a4
Veto Ramirez <vetorobert3@gmail.com>
2020-08-08 09:42:07 -0700

    Finished MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    end = len(arr)
+    start = 0
 
-    # Your code here
-
+    while end > start:
+        middle = (end + start) // 2
+        if target == arr[middle]:
+            return middle
+        elif target < arr[middle]:
+            end = middle
+        elif target > arr[middle]:
+            start = middle
 
     return -1  # not found
