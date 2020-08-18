bf369ff55b0d57c123ec62882177cb4b60ac76ca
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-08-18 12:00:00 -0500

    mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,12 +5,15 @@ def selection_sort(arr):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # (hint, can do in 3 loc)
         # Your code here
 
-
         # TO-DO: swap
         # Your code here
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
@@ -18,9 +21,17 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    swaps_occured = True
+    while swaps_occured:
+        swaps_occured = False
+        for i in range(len(arr) - 1):
+            print(arr)
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i + 1], arr[i]
+                swaps_occured = True
 
     return arr
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +50,9 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
-
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    for i in arr:
+        if i == target:
+            return i
+        else:
+            return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
 
+        if arr[mid] == target:
+            return mid
+        elif target < arr[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
 
     return -1  # not found
