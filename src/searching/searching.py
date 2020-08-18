d763234e461b2603076c0d26c944937174445385
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-08-18 11:45:52 -0700

    tested and wrote linear/binary search.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,27 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
-
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
+    if len(arr) == 0:
+        return -1
 
+    left = 0
+    right = len(arr)
+    while left <= right:
+        mid = int(left + (right - 1) / 2)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
     return -1  # not found
