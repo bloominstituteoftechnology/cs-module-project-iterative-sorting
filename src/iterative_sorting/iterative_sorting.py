190a79915aa04cbd5a75b673d7da9512b6670b1c
Kelson Hartle <kelson99hartle@icloud.com>
2020-09-18 10:06:00 -0600

    Selection sort finished

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,17 +7,25 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for unsorted_index in range(cur_index + 1, len(arr)):
+            if arr[unsorted_index] < arr[smallest_index]:
+                smallest_index = unsorted_index
 
         # TO-DO: swap
         # Your code here
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    
+    # Compare the first and second item of a collection. If the first item is bigger than the second item, swap the items.
+    # Move to the next item. Now, we will compare the second item with the third item. If the second item is bigger than the third, swap the items.
+    # Do this for every item until the end of the list.
+    # Repeat steps 1-3 (decrementing “the end of the list” by 1 each time).
+
 
 
     return arr
