4755535efde4c835bc5a5eee3b1acdeaf2a27c0d
Kelson Hartle <kelson99hartle@icloud.com>
2020-09-18 19:38:46 -0600

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,20 +1,30 @@
 def linear_search(arr, target):
     # Your code here
-    for i in arr:
-        if i == target:
-            return target
+    for i in range(len(arr)):
+        print(range(len(arr)))
+        print(f"{i} -- {arr[i]}")
+        # print(f"LOOPED {i} TARGET {target}")
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
-myArray = [6,7,8,9,8,7]
-linear_search(myArray, 9)
-
-
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-    # Your code here
+    while low <= high:
+        middle = (low + high) // 2
+        guess = arr[middle]
 
+        if guess == target:
+            return middle
 
+        elif guess > target:
+            high = middle - 1
+
+        else:
+            low = middle + 1
 
     return -1  # not found
