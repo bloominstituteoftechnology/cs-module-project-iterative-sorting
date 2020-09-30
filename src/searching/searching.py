a6ef77b27c840a1bbbb7e851bd51b1e325a97704
Conner <0xconr@protonmail.com>
2020-09-30 16:55:24 -0500

    Implemented linear search & tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,9 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+	for i in range(0, len(arr)):
+		if arr[i] == target:
+			return i
+	return -1
 
 
 # Write an iterative implementation of Binary Search
