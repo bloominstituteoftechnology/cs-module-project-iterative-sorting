41e556092d34363c851e625db23fa02808c17281
Matt Martindale <martindalematt18@yahoo.com>
2020-09-29 21:49:54 -0600

    Finished searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
+
 def linear_search(arr, target):
-    # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i # or True
 
-
-    return -1   # not found
+    return -1
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    min = 0
+    max = (len(arr) - 1)
+    found = False
 
-    # Your code here
+    while min <= max and not found:
+        # find the middle of the array
+        middle = ((max + min) // 2)
+        if arr[middle] == target:
+            found = True
+        elif target < arr[middle]:
+            max = middle - 1
+        elif target > arr[middle]:
+            min = middle + 1
 
-
-    return -1  # not found
+    return found
