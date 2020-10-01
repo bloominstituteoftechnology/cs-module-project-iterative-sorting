61fa2710585676fdea25a0487719611eafe085bb
karapeoples <kararpeoples@yahoo.com>
2020-09-30 22:46:43 -0500

    MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,10 +7,16 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
+        #$Find the next smallest element and change the position$#
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index]> arr[j]:
+                smallest_index = j
+            # TO-DO: swap
+            # Your code here
+        #$Swap the elements put the arr on temporary element so swap can happen$#
+        temp = arr[i]
+        arr[i] = arr[smallest_index]
+        arr[smallest_index]= temp
 
     return arr
 
@@ -18,7 +24,16 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+#$Non Recursive Solution$#
+    #$Loop through length from 0 to end$#
+    for p_num in range( len(arr)-1, 0, -1):
+        #$Loop through passed number and bubble$#
+        for i in range(p_num):
+            #$Number at current is greater then next store and swap$#
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
 
     return arr
 
@@ -31,11 +46,11 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
