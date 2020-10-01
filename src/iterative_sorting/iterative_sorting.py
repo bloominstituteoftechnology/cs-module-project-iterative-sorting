b5eb5f43f8f94e3ad5845e945429196d2e8fbd61
Blake Lobato <blakelobato@Blaks-MBP.lan>
2020-10-01 10:34:08 -0700

    modluiterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,6 +4,10 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
@@ -18,6 +22,10 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    for i in range(len(arr) -1):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i, num in enumerate(arr):
+        if num == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    mini = 0
+    med = 0
+    high = len(arr) -1
+    while mini <= high:
+        med = mini + high //2
+        if arr[med] == target:
+            return med
+        elif arr[med] > target:
+            high = med - 1
+        else:
+            mini = med + 1
 
     # Your code here
-
-
     return -1  # not found
