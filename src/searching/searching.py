5db8cb32f482f02eacec7271fd02f6f2a7fd0ffe
hai666l <0xconr@protonmail.com>
2020-09-30 21:38:59 -0500

    searching.py & all tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,8 +8,17 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    offset = 0                          # Offset holds amount trimmed from original array's left side
+    for i in range(0, len(arr)):
+        m = int(len(arr) / 2)           # Calculate middle
 
-    # Your code here
+        if arr[m] == target:            # Found target in array
+            return m+offset             # return index in trimmed array
 
+        if arr[m] > target:             # Compare and trim array
+            arr = arr[:m]
+        else:
+            offset += len(arr[:m])      # Increment offset via what was sliced off the left side of the array
+            arr = arr[m:]
 
-    return -1  # not found
+    return -1                           # Not found
