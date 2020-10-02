6f6ba599ceb02368615c551b9506fe4ca393b7e7
Veto Ramirez <vetorobert3@gmail.com>
2020-10-01 18:32:50 -0700

    working on search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,3 +1,4 @@
+"""
 def linear_search(arr, target):
     # Your code here
     for i in range(len(arr)):
@@ -21,5 +22,40 @@ def binary_search(arr, target):
         elif target > arr[middle]:
             start = middle
 
+
+    return -1  # not found
+"""
+
+# Flexed Code ---------------------------------------------
+
+def linear_search(arr, target):
+    # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return 1
+
+    return -1   # not found
+
+
+# Write an iterative implementation of Binary Search
+def binary_search(arr, target):
+
+    # Your code here
+    first = 0
+    last = (len(arr) - 1)
+    found = -1
+
+    while first <= last and not found:
+
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            found = 1
+
+        else:
+            if target < arr[middle]:
+                last = middle -1
+            else:
+                first = middle + 1
 
     return -1  # not found
