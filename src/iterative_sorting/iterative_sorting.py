5152cc1e208ac05c059195ff99097e0f0ba10c29
Chris Vasquez <chrvasq@outlook.com>
2020-10-04 19:28:15 -0700

    Completed itertative sorting and stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,25 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
-
-        # TO-DO: swap
-        # Your code here
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    list_length = len(arr)
 
-
+    for i in range(list_length):
+        for j in range(0, list_length - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
 '''
@@ -31,16 +31,34 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
-    # Your code here
+    minimum = min(arr, default=maximum)
+    maximum = max(arr, default=maximum)
 
+    if minimum is None:
+        return []
+    elif minimum < 0:
+        return "Error, negative numbers not allowed in Count Sort"
 
-    return arr
+    count = [0 for i in range(maximum + 2)]
+    output = [0 for i in range(len(arr))]
+
+    for i in arr:
+        count[i] += 1
+
+    for i in range(len(count)):
+        count [i] += count[i - 1]
+
+    for i in arr:
+        output[count[i] - 1] = i
+        count[i] -= 1
+
+    return output
