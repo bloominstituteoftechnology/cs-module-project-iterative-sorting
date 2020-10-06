f19f2d6c3049d3b825bf11a35dbf3c3d3c0599bc
Chris Vasquez <chrvasq@outlook.com>
2020-10-05 20:11:26 -0700

    Complete searching mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for index, item in enumerate(arr):
+        if item is target:
+            return index
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-    # Your code here
+    while low <= high:
+        middle = (low + high) // 2
+        guess = arr[middle]
 
-
+        if guess is target:
+            return middle
+        elif guess > target:
+            high = middle - 1
+        else:
+            low = middle + 1
     return -1  # not found
