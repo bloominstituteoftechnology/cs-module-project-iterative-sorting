643f439165b6fd62987bfcb74a613688f1daa24d
Aveline Wingfield <avelinewingfield@gmail.com>
2020-11-02 19:22:54 -0500

    Searching complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
 
+    if target in arr:
+        return arr.index(target)
 
     return -1   # not found
 
@@ -9,6 +11,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr)-1
 
+    while left < right:
+        middle = (left + right) // 2
+        if arr[left] == target:
+            return left
+        if arr[right] == target:
+            return right
+        if arr[middle] == target:
+            return middle
+        if arr[middle] > target:
+            right = middle
+        else:
+            left = middle
 
     return -1  # not found
