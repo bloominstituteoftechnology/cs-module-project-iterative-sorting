b6906901a9d8c358cb0ae1502bc0db6042cbad23
Jason Duncan <jasonduncan@jasons-mbp.myfiosgateway.com>
2020-12-07 19:08:25 -0500

    finshed algos

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,27 +1,37 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
+    length = len(arr)
+    for i in range(0, length):
+        smallest_index = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        if i != smallest_index:
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    length = len(arr)
+    for i in range(length - 1):
+        for j in range(length - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j+1], arr[j] = arr[j], arr[j+1]
+                
 
     return arr
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        value = arr[i]
+        for j in range(i+1, len(arr)):
+            if value < arr[j]:
+                arr[j+1] = arr[j]
+                arr[j] = value
+            else:
+                break
 '''
 STRETCH: implement the Counting Sort function below
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+        
 
 
     return -1   # not found
@@ -8,7 +11,20 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    min = 0
+    max = len(arr) - 1
+    
+    while min <= max:
+        mid = (max + min) // 2
+        
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            max = mid - 1
+        elif target > arr[mid]:
+            min = mid + 1
+            
+    
 
 
     return -1  # not found
